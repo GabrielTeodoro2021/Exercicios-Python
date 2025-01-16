e4f060270c2b2196edb16686675181428d5da88d
Gabriel Teodoro <97770776+GabrielTeodoro2021@users.noreply.github.com>
2025-01-16 00:01:35 -0300

    Update EX_Criar_Arquivo.py

--- EX_Criar_Arquivo.py
+++ EX_Criar_Arquivo.py
@@ -17,5 +17,5 @@ leitura.close
 # r+   Abre o arquivo para leitura e escrita. O arquivo deve já existir. A escrita começa a partir da primeira linha e, caso exista algo escrito no arquivo, as linhas serão reescritas, conforme formos escrevendo.
 # w    Abre o arquivo somente para escrita, no início do arquivo. Apagará o conteúdo do arquivo se ele já existir. Criará um arquivo novo se não existir.
 # w+   Abre o arquivo para escrita e leitura, apagando o conteúdo preexistente.
-# a	   Abre o arquivo para escrita no final do arquivo. Não apaga o conteúdo preexistente.
+# a	  Abre o arquivo para escrita no final do arquivo. Não apaga o conteúdo preexistente.
 # a+   Abre o arquivo para escrita no final do arquivo e leitura.
